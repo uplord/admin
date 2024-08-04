6d2df42ace72e18809d0afc996b7972de5ee7151
Michael <michael@uplord.co.uk>
2024-08-04 19:52:29 +0100

    banner changes

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,42 +1,5 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
-export interface ComponentsSlide extends Schema.Component {
-  collectionName: 'components_components_slides';
-  info: {
-    displayName: 'slide';
-  };
-  attributes: {
-    title: Attribute.String;
-    button: Attribute.Component<'components.button', true>;
-    image: Attribute.Media<'images'>;
-  };
-}
-
-export interface ComponentsSection extends Schema.Component {
-  collectionName: 'components_components_sections';
-  info: {
-    displayName: 'section';
-  };
-  attributes: {
-    title: Attribute.String & Attribute.Required;
-    image: Attribute.Media<'images'>;
-    button: Attribute.Component<'components.button', true>;
-  };
-}
-
-export interface ComponentsButton extends Schema.Component {
-  collectionName: 'components_components_buttons';
-  info: {
-    displayName: 'button';
-  };
-  attributes: {
-    text: Attribute.String & Attribute.Required;
-    url: Attribute.String;
-    class: Attribute.String;
-    target: Attribute.String;
-  };
-}
-
 export interface BlocksSection extends Schema.Component {
   collectionName: 'components_blocks_sections';
   info: {
@@ -60,14 +23,62 @@ export interface BlocksBanner extends Schema.Component {
   };
 }
 
+export interface ComponentsSlide extends Schema.Component {
+  collectionName: 'components_components_slides';
+  info: {
+    displayName: 'slide';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    buttons: Attribute.Component<'components.button', true>;
+    image: Attribute.Media<'images'>;
+    leading: Attribute.String;
+    subtitle: Attribute.String;
+    floating_image: Attribute.Boolean & Attribute.DefaultTo<false>;
+    class: Attribute.String;
+  };
+}
+
+export interface ComponentsSection extends Schema.Component {
+  collectionName: 'components_components_sections';
+  info: {
+    displayName: 'section';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    image: Attribute.Media<'images'>;
+    buttons: Attribute.Component<'components.button', true>;
+    subtitle: Attribute.String;
+    content: Attribute.Blocks;
+    class: Attribute.String;
+  };
+}
+
+export interface ComponentsButton extends Schema.Component {
+  collectionName: 'components_components_buttons';
+  info: {
+    displayName: 'button';
+    description: '';
+  };
+  attributes: {
+    text: Attribute.String & Attribute.Required;
+    url: Attribute.String;
+    class: Attribute.String;
+    target: Attribute.String;
+    file: Attribute.Media<'files'>;
+  };
+}
+
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'blocks.section': BlocksSection;
+      'blocks.banner': BlocksBanner;
       'components.slide': ComponentsSlide;
       'components.section': ComponentsSection;
       'components.button': ComponentsButton;
-      'blocks.section': BlocksSection;
-      'blocks.banner': BlocksBanner;
     }
   }
 }
